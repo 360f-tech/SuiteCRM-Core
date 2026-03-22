42a2d1694a80578d86d597522ab228ec9d23c07b
Erick Pham <erick.pham@360f.com>
2026-03-22 22:36:05 +0700

    chore: clean nginx conf

--- docker/custom_fixes/public/legacy/config_override_template.php
+++ docker/custom_fixes/public/legacy/config_override_template.php
@@ -3,8 +3,10 @@
 // $sugar_config['external_cache']['redis']['port'] = 6379;
 // $sugar_config['external_cache']['redis']['persistent'] = true;
 // $sugar_config['external_cache']['redis']['database'] = 0;
-// $sugar_config['external_cache_disabled_redis'] = false;
-$sugar_config['external_cache_disabled'] = false;
-$sugar_config['external_cache_class'] = 'SugarCacheAPC';
+// $sugar_config['external_cache_disabled_redis'] = false; // set false and add Redis config to enable Redis cache
+$sugar_config['external_cache_disabled'] = false; // default
+$sugar_config['external_cache_class'] = 'SugarCacheAPC'; // default
+$sugar_config['disable_persistent_connections'] = false;
+$sugar_config['developerMode'] = false;
 $sugar_config['http_referer']['list'][] = 'dev-epos-crm.360f.com';
-$sugar_config['http_referer']['actions'] = array('index', 'ListView', 'DetailView', 'EditView', 'oauth', 'authorize', 'Authenticate', 'Login', 'SupportPortal', 'Upgrade');
+// $sugar_config['http_referer']['actions'] = array('index', 'ListView', 'DetailView', 'EditView', 'oauth', 'authorize', 'Authenticate', 'Login', 'SupportPortal', 'Upgrade');
--- docker/custom_packages/Extension/modules/Accounts/Ext/Vardefs/epos_account.php
+++ docker/custom_packages/Extension/modules/Accounts/Ext/Vardefs/epos_account.php
@@ -1,7 +1,7 @@
 <?php
 
 $fields = [
-  'bank_account_holder_name' => ['type' => 'varchar', 'len' => '50'],
+  'bank_account_holder_name' => ['type' => 'varchar', 'len' => '100'],
   'bank_account_number' => ['type' => 'varchar', 'len' => '50'],
   'employer_name' => ['type' => 'varchar', 'len' => '100'],
   'employee_payroll_number' => ['type' => 'varchar', 'len' => '50'],
